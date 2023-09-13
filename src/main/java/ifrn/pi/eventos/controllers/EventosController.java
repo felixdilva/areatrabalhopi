2120543487c68cc56a08b5745afb4f44a9135e7a
Maria Eduarda <mariaeduardadadilva82@gmail.com>
2023-09-13 10:04:04 -0300

    arquivo da atividade 302

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -2,6 +2,7 @@ package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class EventosController {
@@ -9,5 +10,14 @@ public class EventosController {
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return"formEvento";
+	}
+	
+	@RequestMapping(value = "/formrespondido", method = RequestMethod.POST)
+	 public String adicionar (String evento) {
+	 
+		System.out.println("evento: " + evento);
+	 
+	  
+	  return "formSalvo";
 	}
 }
